2bc42447d84087c5e217b6535b1346d7bdbc2584
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-10-01 15:42:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// let str1="sajin is good"
+let bill=("Rs.10")
+console.log("the bill price is:",bill)
+console.log("the length is",bill.length)
+console.log(bill.padEnd(7,"0"))
+
+let bills=("Rs.10")
+console.log("the bills price is:",bills)
+console.log("the length is",bills.length)
+console.log(bills.padStart(7,"0"))
